d5d49a85df273bb64b75bc8d4081dbc62c6ede46
Salvador <salvador.woini@gmail.com>
2023-11-06 02:06:54 -0300

    Liquibase-Example- preconditions change

--- src/main/resources/db/changelog/1.0.0/002-agregar_columnas.sql
+++ src/main/resources/db/changelog/1.0.0/002-agregar_columnas.sql
@@ -1,9 +1,8 @@
 --liquibase formatted sql
 
 -- changeset salvador:002-agregar-columnas
--- preconditions onFail:MARK_RAN onError:MARK_RAN
+-- preconditions onFail:HALT onError:HALT
 -- precondition-sql-check expectedResult:1 select count(*) from pg_tables where tablename = 'character';
--- precondition-sql-check expectedResult:0 select count(*) from information_schema.columns where table_name='character' and column_name='created_at';
 ALTER TABLE public.CHARACTER
     ADD COLUMN CREATED_AT TIMESTAMP NOT NULL;
 ALTER TABLE public.CHARACTER
